c553fba505711aec03544fb1284986ddd7ee0abb
Tatsiana Kuryla <tatianakuryla@gmail.com>
2025-07-31 18:31:23 +0200

    feat: add stylization to the selects

--- src/components/ui/Select.tsx
+++ src/components/ui/Select.tsx
@@ -1,4 +1,5 @@
 import type { SelectProperties } from "../../types.ts";
+import {useRef, useState} from "react";
 
 export const Select = ({
                          className,
@@ -7,19 +8,34 @@ export const Select = ({
                          error,
                          ...rest
                        }: SelectProperties) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  const handleClick = () => {
+    const newState = !isOpen;
+    setIsOpen(newState);
+    ref.current?.classList.toggle('open', newState);
+  };
+
   return (
     <div className="select-wrapper flex">
-      <select
-        className={className}
-        {...rest}
+      <div
+        className="select-wrapper-for-after"
+        ref={ref}
       >
-        {placeholder && <option value="">{placeholder}</option>}
-        {options.map((option) => (
-          <option key={option.value} value={option.value}>
-            {option.label}
-          </option>
-        ))}
-      </select>
+        <select
+          className={className}
+          onClick={() => handleClick()}
+          {...rest}
+        >
+          {placeholder && <option value="">{placeholder}</option>}
+          {options.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+      </div>
       {error && <div className="select-error">{String(error)}</div>}
     </div>
   );
