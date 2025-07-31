f9341b75d7a85be4aaf32bac47e84166d932b1bb
Tatsiana Kuryla <tatianakuryla@gmail.com>
2025-07-31 18:43:34 +0200

    style: implement custom icons for date and time inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import { CreateJobForm } from "./components/form/CreateJobForm.tsx";
-// import { useEffect, useState } from "react";
-// import {initializeSdk} from "./services/initializationSdk.ts";
+import { useEffect, useState } from "react";
+import {initializeSdk} from "./services/initializationSdk.ts";
 
 export function App() {
-  // const [sdkReady, setSdkReady] = useState(false);
-  //
-  // useEffect(() => {
-  //   initializeSdk().then((sdk) => {
-  //     if (sdk) {
-  //       setSdkReady(true);
-  //     }
-  //   });
-  // }, []);
-  //
-  // if (!sdkReady) return <div>Loading Pipedrive SDK...</div>;
+  const [sdkReady, setSdkReady] = useState(false);
+
+  useEffect(() => {
+    initializeSdk().then((sdk) => {
+      if (sdk) {
+        setSdkReady(true);
+      }
+    });
+  }, []);
+
+  if (!sdkReady) return <div>Loading Pipedrive SDK...</div>;
 
   return <CreateJobForm/>;
 }
--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -7,14 +7,22 @@ export const Input = ({
                         error,
                         ...rest
                       }: InputProperties) => {
+  const iconClass =
+    type === 'date' ? 'calendar-icon' :
+      type === 'time' ? 'clock-icon' :
+        '';
+
   return (
     <div className='input-wrapper flex'>
-      <input
-        className={className}
-        type={type}
-        placeholder={placeholder}
-        {...rest}
-      />
+      <div className='input-wrapper-for-icon'>
+        <input
+          className={className}
+          type={type}
+          placeholder={placeholder}
+          {...rest}
+        />
+        {iconClass.length > 0 && <span className={`icon ${iconClass}`} />}
+      </div>
       {error && <div className="input-error">{String(error)}</div>}
     </div>
   );
